32807d02e46a1f68a77e751d992bf3889284241d
zodrigo <rodrigo_pariente@proton.me>
2025-12-24 17:26:58 -0400

    Add exercise 1

---
+++ exercise-1/ex1-extra-credit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* This is a comment*/
+int main()
+{
+    int distance = 100;
+
+    // this is also a comment
+    printf("You are %d miles away.\n", distance);
+    printf("In this book, I'm going to have a small section for each program teaching you how to break the program if it's possible. I'll have you do odd things to the programs, run them in weird ways, or change code so that you can see crashes and compiler errors.\nFor this program, simply try removing things at random and still get it to compile. Just make a guess at what you can remove, recompile it, and then see what you get for an error.");
+}
