9f83fcf93169c22cc28bcc2fe7bee090ee17cff6
Rodrigo Pariente <rodrigo_pariente@proton.me>
2025-12-30 11:31:06 -0400

    Add exercise 7

---
+++ exercise-7/ex7.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main (int argc, char* argv[])
+{
+    int distance = 100;
+    float power = 2.345f; // o que faz o f?
+    double super_power = 56789.4532; // diferenca float x double?
+    char inital = 'A';
+    char first_name[] = "Zed"; // como atribuicao de strings funciona?
+    char last_name[] = "Shaw";
+
+    printf("You are %d miles away.\n", distance);
+    printf("You have %f levels of power.\n", power);
+    printf("You have %f awesome super powers.\n", super_power);
+    printf("I have an initial %c.\n", initial);
+    printf("I have a first name %s.\n", first_name);
+    printf("I have a last name %s.\n", last_name);
+    printf("My whole name is %s %c. %s.\n",
+            first_name, initial, last_name);
+
+    int bugs = 100;
+    double bug_rate = 1.2;
+
+    printf("You have %d bugs at the imaginary rate of %f>\n", bugs, bug_rate);
+
+    long universe_of_defects = 1L * 1024L * 1024L * 1024L;
+    printf("The entire universe has %ld bugs.\n", universe_of_defects);
+
+    double expected_bugs = bugs * bug_rate;
+    printf("You are expected to have %f bugs.\n", expected_bugs);
+
+    double part_of_the_universe = expected_bugs / universe_of_defects;
+    printf("That is only a %e portion of the universe.\n");
+}
